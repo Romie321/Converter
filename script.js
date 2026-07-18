573628efcc6b238cf9d6415a1a5ba03bd0521b96
Romie321 <raguilera321@gmail.com>
2026-07-18 00:30:03 -0700

    Refactor numeral object formatting and improve code readability in convertToRoman function

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
-function convertToRoman(num) {
- let numerals = {
-   1000 : 'M',
-   900 : 'CM',
-   500 : 'D',
-   400 : 'CD',
-   100 : 'C',
-   90 : 'XC',
-   50 : 'L',
-   40 : 'XL',
-   10 : 'X',
-   9 : 'IX',
-   5 : 'V',
-   4 : 'IV',
-   1 : 'I'
- };
+const numInput = document.getElementById(number);
+const numconvert = document.getElementById(convert - btn);
 
- let romanNum = "";
- let numKeys = Object.keys(numerals).map(Number).sort((a, b) => b - a);
- 
- for(let i = 0; i < numKeys.length; i++) {
-   let key = numKeys[i];
-   while(key <= num) {
-     romanNum += numerals[key];
-     num -= key
-   }
- }
- return romanNum;
+function convertToRoman(num) {
+  let numerals = {
+    1000: "M",
+    900: "CM",
+    500: "D",
+    400: "CD",
+    100: "C",
+    90: "XC",
+    50: "L",
+    40: "XL",
+    10: "X",
+    9: "IX",
+    5: "V",
+    4: "IV",
+    1: "I",
+  };
+
+  let romanNum = "";
+  let numKeys = Object.keys(numerals)
+    .map(Number)
+    .sort((a, b) => b - a);
+
+  for (let i = 0; i < numKeys.length; i++) {
+    let key = numKeys[i];
+    while (key <= num) {
+      romanNum += numerals[key];
+      num -= key;
+    }
+  }
+  return romanNum;
 }
 
 console.log(convertToRoman(36));
